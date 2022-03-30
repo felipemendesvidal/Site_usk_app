59f1b378e62d4a79d8ad5da3fb191d2cfff4ebaa
felipe vidal <contatofelipevidalmendes@gmail.com>
2022-03-30 11:16:53 -0300

    js finalizado e funcionando da galeria

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,16 @@ function toogleMenu (){
 }
 
 btnMobile.addEventListener('click', toogleMenu);
+
+
+
+//galeria
+document.querySelectorAll('.image-container img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector( '.popup-image').style.display = 'block';
+        document.querySelector( '.popup-image img').src = image.getAttribute('src');
+    }
+});
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector( '.popup-image').style.display = 'none';
+}
